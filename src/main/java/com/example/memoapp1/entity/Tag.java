4e74b17cdc6065d4813a3d9f57c422e5270ab265
SHOTA <subaseball820@gmail.com>
2025-10-05 20:33:35 +0900

    タグエンティティの作成

--- src/main/java/com/example/memoapp1/entity/Tag.java
+++ src/main/java/com/example/memoapp1/entity/Tag.java
@@ -1,5 +1,35 @@
 package com.example.memoapp1.entity;
 
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tag")
 public class Tag {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Memo> memos = new HashSet<>();
+
+    // getter/setter
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public boolean isDeleted() { return isDeleted; }
+    public void setDeleted(boolean deleted) { isDeleted = deleted; }
+
+    public Set<Memo> getMemos() { return memos; }
+    public void setMemos(Set<Memo> memos) { this.memos = memos; }
 }
