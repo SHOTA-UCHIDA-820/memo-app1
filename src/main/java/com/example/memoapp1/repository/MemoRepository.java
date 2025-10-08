2c84f42ef62affcf73aaa4044183acdd47d40128
SHOTA <subaseball820@gmail.com>
2025-10-08 09:17:41 +0900

    メモリポジトリの作成

--- src/main/java/com/example/memoapp1/repository/MemoRepository.java
+++ src/main/java/com/example/memoapp1/repository/MemoRepository.java
@@ -1,5 +1,17 @@
 package com.example.memoapp1.repository;
 
-public class MemoRepository {
+import com.example.memoapp1.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    @Query("SELECT m FROM Memo m LEFT JOIN FETCH m.tags WHERE m.id = :id")
+    Memo findByIdWithTags(@Param("id") Long id);
+
+    @Query("SELECT DISTINCT m FROM Memo m LEFT JOIN FETCH m.tags")
+    List<Memo> findAllWithTags();
 }
