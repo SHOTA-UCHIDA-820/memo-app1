37fdcb5f8e38bfd1986da523644b49a568766f56
SHOTA <subaseball820@gmail.com>
2025-11-09 16:45:30 +0900

    コードの可読性の向上

--- src/main/java/com/example/memoapp1/entity/Tags.java
+++ src/main/java/com/example/memoapp1/entity/Tags.java
@@ -11,7 +11,6 @@ public class Tags {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
 
     @Column(name = "is_deleted")
--- src/main/java/com/example/memoapp1/repository/TagRepository.java
+++ src/main/java/com/example/memoapp1/repository/TagRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tags, Long> {
-
-    List<Tags> findByIsDeletedFalse();
+    
+	List<Tags> findByIsDeletedFalse();	
 }
